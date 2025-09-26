bf5848e241e16d25f3cf8c802bae246f94cbd845
Mouhamed Kairson Coundoul <mcoundoul6@gmail.com>
2025-09-26 14:17:28 -0400

    chore(#18): initialize monorepo with Turborepo and scaffold Expo TypeScript app

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import rn from "eslint-plugin-react-native";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx,js,jsx}"],
+    ignores: ["**/node_modules/**","**/dist/**","**/build/**",".expo/**"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: tsParser,
+      parserOptions: { ecmaFeatures: { jsx: true } },
+      globals: { ...globals.browser, ...globals.es2021 }
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      react,
+      "react-hooks": reactHooks,
+      "react-native": rn
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["warn",{ argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "react-native/no-unused-styles": "warn",
+      "react-native/no-inline-styles": "off"
+    },
+    settings: { react: { version: "detect" } }
+  }
+];
