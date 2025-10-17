e7015215f66e09ba4818da79a0caea0ce0aaa948
MomoG221 <146865335+MomoG221@users.noreply.github.com>
2025-10-16 21:47:25 -0400

    chore(#283): update frontend (mobile) jest.config.js to ensure test coverage is measured correctly

--- apps/mobile/jest.config.js
+++ apps/mobile/jest.config.js
@@ -2,4 +2,14 @@ module.exports = {
   preset: 'jest-expo',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  collectCoverageFrom: [
+    '*.{ts,tsx}',
+    'app/**/*.{ts,tsx}',
+    'components/**/*.{ts,tsx}',
+    'sentry/**/*.{ts,tsx}',
+    '!**/*.d.ts',
+    '!**/test/**',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
 };
