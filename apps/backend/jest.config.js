748d65098f2d57a8385613cf15c84be25686f1ad
MomoG221 <146865335+MomoG221@users.noreply.github.com>
2025-10-20 09:17:13 -0400

    fix(#277): fix failing test in the pipeline

--- apps/backend/jest.config.js
+++ apps/backend/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>/src', '<rootDir>/test'],
   testMatch: ['**/test/**/*.test.ts', '**/*.test.ts'],
   collectCoverageFrom: [
     'src/**/*.ts',
