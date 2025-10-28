a441612ee43e2342670347912e91bb41f6de476a
Mouhamed Kairson Coundoul <mcoundoul6@gmail.com>
2025-10-27 23:26:33 -0400

    Add comprehensive unit tests for HomeScreen, BottomTabNavigator, and service modules; enhance Jest configuration for coverage reporting

--- apps/mobile/jest.config.js
+++ apps/mobile/jest.config.js
@@ -6,4 +6,18 @@ module.exports = {
     '^expo/src/winter/ImportMetaRegistry$': '<rootDir>/test/mocks/expoImportMetaRegistry.ts',
     '^expo/src/winter$': '<rootDir>/test/mocks/expoWinter.ts',
   },
+  collectCoverageFrom: [
+    '<rootDir>/**/*.{ts,tsx}',
+    '!<rootDir>/**/__mocks__/**',
+    '!<rootDir>/**/test/**',
+    '!<rootDir>/**/coverage/**',
+    '!<rootDir>/**/dist/**',
+    '!<rootDir>/**/node_modules/**',
+    '!<rootDir>/**/*.d.ts',
+    '!<rootDir>/**/metro.config.js',
+    '!<rootDir>/**/jest.config.js',
+    '!<rootDir>/**/jest.setup.js',
+    '!<rootDir>/**/tailwind.config.js',
+    '!<rootDir>/**/babel.config.js',
+  ],
 };
