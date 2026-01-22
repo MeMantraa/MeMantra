b0c1b69a6402d53d15c33105175fdfabc5333b3c
nicpuertas <nicpuertas@gmail.com>
2026-01-22 13:15:10 -0500

    slight fixes to db config

--- apps/backend/src/config/db.config.ts
+++ apps/backend/src/config/db.config.ts
@@ -4,20 +4,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const pool = new Pool({
-  // host: process.env.DB_HOST,
-  // port: Number.parseInt(process.env.DB_PORT || '5432'),
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_NAME,
-
-  connectionString: process.env.DATABASE_URL, // Neon Hosted Database URL
-  ssl: { rejectUnauthorized: false }, // required for Neon
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 export async function testConnection() {
   try {
     const client = await pool.connect();
-    console.log('Successfully connected to the Neon hosted shared database');
+    console.log('Successfully connected to the database');
     client.release();
     return true;
   } catch (error) {
