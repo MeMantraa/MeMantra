288f087c6143fc9e8121cda4430959aba995bed7
Steven Gourgy <stevengourgy@hotmail.com>
2026-03-31 00:20:32 -0400

    feat(#545)-fix deprecated library

--- apps/mobile/utils/photoUpload.ts
+++ apps/mobile/utils/photoUpload.ts
@@ -14,7 +14,7 @@ export const photoUploadUtils = {
 
   async pickImageFromLibrary(): Promise<string | null> {
     const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: ['images'],
       allowsEditing: true,
       aspect: [1, 1],
       quality: 0.4,
